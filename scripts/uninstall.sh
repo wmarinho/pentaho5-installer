0197095acaf18137c5a1f318366a0c6603e7ea4a
Wellington Marinho <root@ip-172-31-16-168.sa-east-1.compute.internal>
2014-03-16 00:47:36 +0000

    .

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,6 +3,7 @@ read -p "Tem certeza que deseja desistalar o Pentaho? (y/n) " yn
 if [ "$yn" == "y" ] || [ "$yn" == "Y" ]; then
 	read -p "****** ATENÇÃO: TODOS OS ARQUIVOS SERÃO EXCLUÍDOS. CONFIRMA? (y/n): " y
         if [ "$y" == "y" ] || [ "$y" == "Y" ]; then
-		userdel -f -r pentaho
+#		userdel -f -r pentaho
+		echo "Ainda não implementado"
 	fi
 fi
